11957622f64ab07ccbdfa7b3f83dc6c864846965
Ignacio Ezequiel Taboada Ojeda <406983@sistemas.frc.utn.edu.ar>
2026-02-05 15:10:15 -0300

    arreglos de imagenes y index.html

--- src/layout/Navigation.jsx
+++ src/layout/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import logo from '../assets/logo-kuuii-marca.jpg'; 
+import logo from '../assets/logo-kuuii-marca.png'; 
 import './Navigation.css';
 
 const Navigation = () => {
