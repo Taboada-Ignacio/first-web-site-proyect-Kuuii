85cbe0a930ca7d0bf67a39041024a1beb52f2821
Ignacio Ezequiel Taboada Ojeda <406983@sistemas.frc.utn.edu.ar>
2026-02-05 18:23:51 -0300

    mejora en Best Practices

--- src/layout/Navigation.jsx
+++ src/layout/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import logo from '../assets/logo-kuuii-marca.png'; 
+import logo from '../assets/logo-kuuii-marca1.png'; 
 import './Navigation.css';
 
 const Navigation = () => {
